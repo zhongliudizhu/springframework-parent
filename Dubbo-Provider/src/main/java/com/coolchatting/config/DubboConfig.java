e035554a4620603a86f1cd7ae1a5959f1f977ff3
shibinbin <15596186979@163.com>
2019-07-22 14:37:01 +0800

    去除空模块

--- Dubbo-Provider/src/main/java/com/coolchatting/config/DubboConfig.java
+++ Dubbo-Provider/src/main/java/com/coolchatting/config/DubboConfig.java
@@ -1,11 +1,7 @@
 package com.coolchatting.config;
 
-import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
-import com.alibaba.dubbo.config.spring.context.annotation.EnableDubboConfig;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
--- Spring-Ioc/src/main/java/com/coolchatting/Bootstrap.java
+++ Spring-Ioc/src/main/java/com/coolchatting/Bootstrap.java
@@ -1,11 +1,8 @@
 package com.coolchatting;
 
-import com.coolchatting.beanfactorypostprocessor.MyBeanDefinitionPostProcessor;
 import com.coolchatting.conf.BeansConf;
 import com.coolchatting.domain.Order;
 import com.coolchatting.domain.User;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
